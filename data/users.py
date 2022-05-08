f185e50d1b0d882ef54dc288c310b659c509e176
Mal1gos <94733442+Mal1gos@users.noreply.github.com>
2022-05-08 13:01:43 +0500

    Add files via upload

--- data/users.py
+++ data/users.py
@@ -5,9 +5,15 @@ from sqlalchemy import orm
 
 
 class User(SqlAlchemyBase):
+
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
+
                            primary_key=True, autoincrement=True)
+
+    name_channel = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
     message = sqlalchemy.Column(sqlalchemy.String, nullable=True)
