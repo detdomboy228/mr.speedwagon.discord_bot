92187a804591b3dc046e95d459f3b1e44530a191
detdomboy228 <94791820+detdomboy228@users.noreply.github.com>
2022-08-25 15:39:51 +0500

    Update fate.py

--- fate.py
+++ fate.py
@@ -32,10 +32,8 @@ logger.setLevel(logging.WARNING)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='-', intents=intents)
+client = discord.Client()
+bot = commands.Bot(command_prefix='-')
 YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': 'False', 'simulate': 'True',
                'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio',
                'logger': logger}
